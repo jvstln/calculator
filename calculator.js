8fb70b0d0e079134771b2a63c37a33ad33be791f
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-14 04:47:43 +0100

    Create basic math functions
    add, subtract, multiply, divide

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function add(...numbers)  {
+  return numbers.reduce((sum, number) => sum + number, 0);
+}
+
+function subtract(...numbers)  {
+  if (numbers.length == 0) return 0;
+  return numbers.reduce((difference, number) => difference - number);
+}
+
+function multiply(...numbers) {
+  return numbers.reduce((product, number) => product * number, 1);
+}
+
+function divide(...numbers) {
+  if (numbers.length == 0) return 0;
+  return numbers.reduce((quotient, number) => quotient / number);
+}
