d53e16b829939f3f64db4e758e577677211ff113
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-15 17:40:57 +0100

    Define calculator tokenTypes
    token is the smallest part of an expression
    examples 1, ., +
    
    Added helper function to check type of tokens

--- calculator.js
+++ calculator.js
@@ -1,8 +1,28 @@
 const calculator = {
   operatorFunctions: {
-    "+": (num1 = 0, num2 = 0) => num1 + num2,
-    "-": (num1 = 0, num2 = 0) => num1 - num2,
-    "*": (num1 = 0, num2 = 1) => num1 * num2,
-    "/": (num1 = 0, num2 = 1) => num1 / num2,
+    "+": (num1, num2) => (num2 == undefined ? Number(num1) : num1 + num2),
+    "-": (num1, num2) => (num2 == undefined ? Number(num1) : num1 - num2),
+    "*": (num1, num2 = 1) => num1 * num2,
+    "/": (num1, num2 = 1) => num1 / num2,
+    "%": (num = 0) => num / 100,
+  },
+  tokenTypes: {
+    preUnary: ["+", "-"],
+    postUnary: ["%"],
+    binary: ["+", "-", "*", "/"],
+    operand: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."],
+  },
+
+  isOperand(token) {
+    return this.tokenTypes.operand.includes(token);
+  },
+  isPreUnary(token) {
+    return this.tokenTypes.preUnary.includes(token);
+  },
+  isPostUnary(token) {
+    return this.tokenTypes.postUnary.includes(token);
+  },
+  isBinary(token) {
+    return this.tokenTypes.binary.includes(token);
   },
 };
