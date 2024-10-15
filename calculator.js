13aee8bb1c20f5703095e5e3e931db6fc2c1cb31
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-15 04:16:04 +0100

    Create calculator object containing math functions

--- calculator.js
+++ calculator.js
@@ -1,17 +1,8 @@
-function add(...numbers)  {
-  return numbers.reduce((sum, number) => sum + number, 0);
-}
-
-function subtract(...numbers)  {
-  if (numbers.length == 0) return 0;
-  return numbers.reduce((difference, number) => difference - number);
-}
-
-function multiply(...numbers) {
-  return numbers.reduce((product, number) => product * number, 1);
-}
-
-function divide(...numbers) {
-  if (numbers.length == 0) return 0;
-  return numbers.reduce((quotient, number) => quotient / number);
-}
+const calculator = {
+  operatorFunctions: {
+    "+": (num1 = 0, num2 = 0) => num1 + num2,
+    "-": (num1 = 0, num2 = 0) => num1 - num2,
+    "*": (num1 = 0, num2 = 1) => num1 * num2,
+    "/": (num1 = 0, num2 = 1) => num1 / num2,
+  },
+};
